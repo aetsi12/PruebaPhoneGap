5c736ad946e963af00f4e195723992d61c36837e
Dait <daitdoce@gmail.com>
2017-01-13 02:08:44 +0100

    Importar fotos

--- js/camera.js
+++ js/camera.js
@@ -1,16 +1,45 @@
 var app = {
     inicio: function(){
         this.iniciaFastClick();
-        this.iniciaBoton();
+        this.iniciaBotones();
     },
     iniciaFastClick: function(){
         FastClick.attach(document.body);
     },
-    iniciaBoton: function(){
+    iniciaBotones: function(){
         var buttonAction = document.querySelector('#button-action');
-        buttonAction.addEventListener('click', this.tomarFoto);
+        buttonAction.addEventListener('click', function(){
+            app.cargarFoto(Camera.PictureSourceType.CAMERA);
+        });
+
+        var buttonGallery = document.querySelector('#button-gallery');
+        buttonGallery.addEventListener('click', function(){
+            app.cargarFoto(Camera.PictureSourceType.PHOTOLIBRARY);
+        });
+
+        var filterButtons = document.querySelectorAll('.button-filter');
+        filterButtons[0].addEventListener('click', function(){
+            app.aplicaFiltro('gray');
+        });
+        filterButtons[1].addEventListener('click', function(){
+            app.aplicaFiltro('negative');
+        });
+        filterButtons[2].addEventListener('click', function(){
+            app.aplicaFiltro('sepia');
+        });
     },
-    tomarFoto: function(){
+    cargarFoto: function(type){
+        var opciones = {
+            quality: 50,
+            sourceType: type,
+            destinationType: Camera.DestinationType.FILE_URI,
+            targetWidth: 300,
+            targetHeight: 300,
+            correctOrientation: true
+        };
+        navigator.camera.getPicture(app.fotoCargada, app.errorFoto, opciones);
+    },
+    /*tomarFoto: function(){
         var opciones = {
             quality: 50,
             destinationType: Camera.DestinationType.FILE_URI,
@@ -19,16 +48,36 @@ var app = {
             correctOrientation: true
         };
         navigator.camera.getPicture(app.fotoTomada, app.errorFoto, opciones);
+    },*/
+    fotoCargada: function(imageURI){
+        var img = document.createElement('img');
+        img.onload = function(){
+            app.pintarFoto(img);
+        }
+        img.src = imageURI;
     },
-    fotoTomada: function(imageURI){
-        var image = document.querySelector('#foto');
-        image.src = imageURI;
+    pintarFoto: function(img){
+        var canvas = document.querySelector('#foto');
+        var context = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img,0,0,img.width,img.height);
     },
     errorFoto: function(message){
         console.log("Fallo al sacar la foto o foto cancelada: "+message);
+    },
+    aplicaFiltro: function(filtro){
+        var canvas = document.querySelector('#foto');
+        var context = canvas.getContext('2d');
+
+        imageData = context.getImageData(0,0,canvas.width,canvas.height);
+
+        effects[filtro](imageData.data);
+        context.putImageData(imageData,0,0);
     }
 };
 
+var imageData;
 if('addEventListener' in document){
     document.addEventListener('DOMContentLoaded', function(){
         app.inicio();
