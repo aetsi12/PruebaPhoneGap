63b122bdc833b1ee972c37c1836a175c30fae489
Dait <daitdoce@gmail.com>
2017-01-14 00:26:50 +0100

    Markador de tu posición actual + marcadores al clickar por el mapa

--- js/maps.js
+++ js/maps.js
@@ -14,6 +14,17 @@ var app = {
         L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYm9jYXRhMTIiLCJhIjoiY2l4d2VuaG91MDAzajMybm9icHA3bXQ2YiJ9.PDNsY80mvHO7Ru0HH8Ha1g',{
             maxZoom: 18
         }).addTo(miMapa);
+
+        app.pintarMarcador([position.coords.latitude, position.coords.longitude], '¡Estoy aquí!', miMapa);
+
+        miMapa.on('click', function(event){
+            var txt = 'Marcador en l(' + event.latlng.lat.toFixed(2) + ') y L(' + event.latlng.lng.toFixed(2) + ').';
+            app.pintarMarcador(event.latlng, txt, miMapa);
+        });
+    },
+    pintarMarcador: function(latlng, txt, mapa){
+        var marcador = L.marker(latlng).addTo(mapa);
+        marcador.bindPopup(txt).openPopup();
     },
     error: function(e){
         console.log(e.code + ": " + e.message);
