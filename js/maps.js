28167c576a52ec74d4446a1e708eb046c4fee538
Dait <daitdoce@gmail.com>
2017-01-14 00:13:36 +0100

    Mapa con Leaflet y Mapbox (aunque no carga e.e)

--- js/maps.js
+++ js/maps.js
@@ -9,12 +9,15 @@ var app = {
         navigator.geolocation.getCurrentPosition(app.dibujaCoordenadas, app.error);
     },
     dibujaCoordenadas: function(position){
-        var coordsDiv = document.querySelector('#coords');
-        coordsDiv.innerHTML = "Latitud: "+position.coords.latitude + "Longitud: "+position.coords.longitude;
+        var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+
+        L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYm9jYXRhMTIiLCJhIjoiY2l4d2VuaG91MDAzajMybm9icHA3bXQ2YiJ9.PDNsY80mvHO7Ru0HH8Ha1g',{
+            maxZoom: 18
+        }).addTo(miMapa);
     },
     error: function(e){
         console.log(e.code + ": " + e.message);
-        document.querySelector('#coords').innerHTML = e.code + ": " + e.message;
+        document.querySelector('#map').innerHTML = e.code + ": " + e.message;
     }
 };
 
