2e2e4e51af2572d08a8b8c9218478128c8655a83
Dait <daitdoce@gmail.com>
2017-01-14 00:51:41 +0100

    Detectar agitación (x > 10 || y > 10)

--- js/sensores.js
+++ js/sensores.js
@@ -7,9 +7,18 @@ var app = {
     },
 
     onSuccess: function(data){
+        app.detectaAgitacion(data);
         app.representa(data.x, '#valorx');
         app.representa(data.y, '#valory');
         app.representa(data.z, '#valorz');
+    },
+    detectaAgitacion: function(data){
+        agitacionX = data.x > 10;
+        agitacionY = data.y > 10;
+        if(data.x > 10 || data.y > 10)
+            document.body.className = 'agitado';
+        else
+            document.body.className = '';
     },
     representa: function(data, htmlElement)  {
         data = Math.round(data * 100) / 100;
